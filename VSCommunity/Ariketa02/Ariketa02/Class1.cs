0af6264ccdc042defbba0a3d27e7429dfc867a9b
Gorka Garcia <garcia.gorka@uni.eus>
2023-09-21 13:19:01 +0200

    Start Ariketa02

---
+++ VSCommunity/Ariketa02/Ariketa02/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ariketa02
+{
+    internal class Class1
+    {
+    }
+}
