c0303dcd7b8e2f56e126c97931605a85a59bbf36
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-10 12:39:33 +0200

    Update second chart

--- VSCommunity/AginteKoadroa/AginteKoadroa/AginteKoadroa/Form1.cs
+++ VSCommunity/AginteKoadroa/AginteKoadroa/AginteKoadroa/Form1.cs
@@ -51,7 +51,6 @@ namespace AginteKoadroa
                         var kontrolak = userControl11.Controls.OfType<System.Windows.Forms.DataVisualization.Charting.Chart>();
                         foreach (var kontrola in kontrolak)
                         {
-                            kontrola.Titles[0].Text = "SALMENTA GEHIENGO BEZEROAK";
                             kontrola.DataSource = salmentaData;
                             kontrola.Series[0].YValueMembers = "Value";
                             kontrola.Series[0].XValueMember = "Key";
