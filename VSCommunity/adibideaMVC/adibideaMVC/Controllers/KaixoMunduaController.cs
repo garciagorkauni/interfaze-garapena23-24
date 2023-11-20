636ff4b951538529617e845c73b7181f72ec9d3c
Gorka Garcia <garcia.gorka@uni.eus>
2023-11-20 09:57:45 +0100

    First MVC project created

---
+++ VSCommunity/adibideaMVC/adibideaMVC/Controllers/KaixoMunduaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace adibideaMVC.Controllers
+{
+    public class KaixoMunduaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string OngiEtorri(string izena, int zenbat = 1)
+        {
+            return HttpUtility.HtmlEncode("Kaixo " + izena + ", zenbat da: " + zenbat);
+        }
+    }
+}
