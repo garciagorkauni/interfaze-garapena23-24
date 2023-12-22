88d605146d458fb29f7b3fa92f027821e30ba519
Gorka Garcia <garcia.gorka@uni.eus>
2023-12-22 09:20:17 +0100

    Adibide proiektua bukatuta

--- VSCommunity/WineShop/WineShop/WineShop/obj/Debug/net6.0/WineShop.AssemblyInfo.cs
+++ VSCommunity/WineShop/WineShop/WineShop/obj/Debug/net6.0/WineShop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+341ad778490e2541555cd68ff2b3a17aac934dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7082e964b90e7075e64d224899b86f01335bb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WineShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
